7c404d3fcb52f0f1cbdffc474bfd2ce1c34c03f5
herbatniczek0 <115565033+herbatniczek0@users.noreply.github.com>
2025-06-19 20:37:23 +0200

    Update getSubmission.js

--- functions/getSubmission.js
+++ functions/getSubmission.js
@@ -4,19 +4,21 @@ const sqlGet = neonGet(process.env.NETLIFY_DATABASE_URL);
 exports.handler = async (event) => {
   const { id } = event.queryStringParameters;
   try {
-    const result = await sqlGet`SELECT * FROM submissions WHERE id = ${id}`;
+   const result = await sqlGet`SELECT * FROM submissions WHERE id = ${id}`;
+const rows = result.rows;
 
-    if (result.length === 0) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: 'Nie znaleziono wniosku' })
-      };
-    }
+if (rows.length === 0) {
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ error: 'Nie znaleziono wniosku' })
+  };
+}
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result[0])
-    };
+return {
+  statusCode: 200,
+  body: JSON.stringify(rows[0]) // <-- poprawnie
+};
+
   } catch (err) {
   console.error("Błąd zapisu:", err, "getSubmission.js"); // <-- dodaj to
     return {
