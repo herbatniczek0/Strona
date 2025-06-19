975529e44ecb398918b0f44e6461add6d5376881
herbatniczek0 <115565033+herbatniczek0@users.noreply.github.com>
2025-06-19 20:37:01 +0200

    Update getConsultations.js

--- functions/getConsultations.js
+++ functions/getConsultations.js
@@ -3,11 +3,12 @@ const sqlConsult = neonConsult(process.env.NETLIFY_DATABASE_URL);
 
 exports.handler = async () => {
   try {
-    const result = await sqlConsult`SELECT * FROM consultations`;
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result)
-    };
+const result = await sqlConsult`SELECT * FROM consultations`;
+return {
+  statusCode: 200,
+  body: JSON.stringify(result.rows) // <-- to!
+};
+
   } catch (err) {
   console.error("Błąd zapisu:", err, "getConsultations.js"); // <-- dodaj to
     return {
