a3d4a29efce511b1ab56f5ff511dddd4e4fe97ea
herbatniczek0 <115565033+herbatniczek0@users.noreply.github.com>
2025-06-24 00:15:20 +0200

    Update users.js

--- functions/users.js
+++ functions/users.js
@@ -1,14 +1,14 @@
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
 exports.handler = async (event) => {
   if (event.httpMethod !== 'GET') {
     return { statusCode: 405, body: JSON.stringify({ message: 'Method not allowed' }) };
   }
+
+  const client = new Client({
+    connectionString: process.env.NETLIFY_DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
 
   try {
     await client.connect();
@@ -22,9 +22,13 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ users: res.rows }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' },
     };
   } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ message: 'Błąd serwera', error: error.message }) };
+    if(client) await client.end();
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Błąd serwera', error: error.message }),
+    };
   }
 };
